2f7ab2e7b5d0191f1e4cbec29dd8d887478005c0
Matteo Magnini <matteo.magnini@unibo.it>
2024-10-29 14:14:00 +0100

    feat: added automatic generation of relation_info

--- relation_info_generator.py
+++ relation_info_generator.py
@@ -10,6 +10,8 @@ from relation_info import PATH as PATH_RELATION_INFO
 
 PATTERN_SYMBOLS = re.compile(r"[^A-Za-z\d_]")
 PATH_ONTOLOGY = PATH_ONTOLOGY / "ontology.owl"
+MAX_NUMBER_SEED_ENT_TUPLES = 5
+SEED = 753
 
 
 def first(iterable: typing.Iterable[typing.Any]) -> typing.Any:
@@ -188,13 +190,33 @@ def name_to_snake_case(name: str) -> str:
 
 
 if __name__ == "__main__":
+    processed = set()
     json_config_file = "{\n"
     with KnowledgeGraph() as kg:
         Recipe = kg.onto.Recipe
         for cls in kg.visit_classes_depth_first():
-            json_config_file += '\t"is_instance_of_{}": [\n\t\t'.format(name_to_snake_case(human_name(cls)))
-            json_config_file += '"<ENT0> is instance of {} ."\n\t'.format(human_name(cls))
+            if human_name(cls) in processed:
+                continue
+            processed.add(human_name(cls))
+            json_config_file += '\t"is_instance_of_{}": '.format(name_to_snake_case(human_name(cls))) + '{\n\t\t'
+            json_config_file += '"init_prompts": [\n\t\t\t'
+            json_config_file += '"<ENT0> is instance of {} ."\n\t\t'.format(human_name(cls))
             json_config_file += "],\n"
+            # Generate the seed entity tuples (random pick from the instances of the class)
+            instances = list(cls.instances())
+            # Random re-arrange the instances
+            instances = sorted(instances, key=lambda x: hash(x.name + str(SEED)))
+            json_config_file += '\t\t"seed_ent_tuples": [\n'
+            number_of_tuples = min(len(instances), MAX_NUMBER_SEED_ENT_TUPLES)
+            for i in range(number_of_tuples):
+                instance = instances[i]
+                json_config_file += '\t\t\t[\n\t\t\t\t"{}"\n\t\t\t],\n'.format(instance.name)
+            # remove the last comma
+            json_config_file = json_config_file[:-2] + "\n"
+            json_config_file += "\t\t]\n"
+            json_config_file += '\t},\n'
+        # remove the last comma
+        json_config_file = json_config_file[:-2] + "\n"
         json_config_file += '}'
 
     # Save the JSON configuration file
