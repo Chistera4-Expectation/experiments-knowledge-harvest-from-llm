5e0668c3a114ea72436d9cecdae7f5ee4339578e
Matteo Magnini <matteo.magnini@unibo.it>
2024-10-31 11:46:24 +0100

    fix: correct way to generate the ontology

--- build_ontology.py
+++ build_ontology.py
@@ -2,6 +2,7 @@ from utils import KnowledgeGraph, human_name, name_to_snake_case
 from ontologies import PATH as PATH_ONTOLOGY
 from results import PATH as PATH_RESULTS
 import json
+import os
 
 
 PATH_RESULTS = PATH_RESULTS / "nutrition" / "100tuples_top20prompts" / "roberta-large"
@@ -12,28 +13,27 @@ THRESHOLD = 10E-3
 
 if __name__ == "__main__":
     processed_classes = set()
-    with KnowledgeGraph(PATH_BASE_ONTOLOGY) as kg:
-        kg.onto.save(str(PATH_POPULATED_ONTOLOGY))
-        with KnowledgeGraph(PATH_POPULATED_ONTOLOGY) as kg_populated:
-            Recipe = kg.onto.Recipe
-            for cls in kg.visit_classes_depth_first():
-                if human_name(cls) in processed_classes:
-                    continue
-                processed_classes.add(human_name(cls))
-                # Read the results file for the specific class
-                results_file = PATH_RESULTS / "is_instance_of_{}".format(name_to_snake_case(human_name(cls))) / "ent_tuples.json"
-                # Iterate over the results file
-                with open(results_file, "r") as f:
-                    results = json.load(f)
-                # Add each entity to the class
-                processed_entities = set()
-                for entity in results:
-                    for ent in entity:
-                        if isinstance(ent, float):
-                            continue
-                        if ent[0] not in processed_entities:
-                            if entity[1] > THRESHOLD:
-                                kg_populated.add_instance(cls, ent[0])
-                                processed_entities.add(ent[0])
-            # Save the populated ontology
-            kg_populated.save()
+    os.system("cp {} {}".format(str(PATH_BASE_ONTOLOGY), str(PATH_POPULATED_ONTOLOGY)))
+    with KnowledgeGraph(PATH_POPULATED_ONTOLOGY) as kg:
+        Recipe = kg.onto.Recipe
+        for cls in kg.visit_classes_depth_first():
+            if human_name(cls) in processed_classes:
+                continue
+            processed_classes.add(human_name(cls))
+            # Read the results file for the specific class
+            results_file = PATH_RESULTS / "is_instance_of_{}".format(name_to_snake_case(human_name(cls))) / "ent_tuples.json"
+            # Iterate over the results file
+            with open(results_file, "r") as f:
+                results = json.load(f)
+            # Add each entity to the class
+            processed_entities = set()
+            for entity in results:
+                for ent in entity:
+                    if isinstance(ent, float):
+                        continue
+                    if ent[0] not in processed_entities:
+                        if entity[1] > THRESHOLD:
+                            kg.add_instance(cls, ent[0])
+                            processed_entities.add(ent[0])
+        # Save the populated ontology
+        kg.save()
--- is_ingredient_of_generator.py
+++ is_ingredient_of_generator.py
@@ -1,34 +1,54 @@
 from utils import KnowledgeGraph, human_name, name_to_snake_case, subtype
 from relation_info import PATH as PATH_RELATION_INFO
+from ontologies import PATH as PATH_ONTOLOGY
+
+
+PATH_ONTOLOGY_POPULATED = PATH_ONTOLOGY / "populated_ontology.owl"
+PATH_ONTOLOGY_AUXILIARY = PATH_ONTOLOGY / "ontology.owl"
+MAX_NUMBER_SEED_ENT_TUPLES = 5
+SEED = 753
 
 
 if __name__ == "__main__":
     processed = set()
     json_config_file = "{\n"
-    with KnowledgeGraph() as kg:
-        Recipe = kg.onto.Recipe
-        for cls in kg.visit_classes_depth_first():
-            if human_name(cls) in processed:
-                continue
-            if not subtype(cls, Recipe):
-                continue
-            processed.add(human_name(cls))
-            # Retrieve all the recipes for the class
-            instances = list(cls.instances())
-            for recipe in instances:
-                if human_name(recipe) in processed:
+    with KnowledgeGraph(PATH_ONTOLOGY_POPULATED) as kg:
+        with KnowledgeGraph(PATH_ONTOLOGY_AUXILIARY) as kg_aux:
+            Recipe = kg.onto.Recipe
+            for cls in kg.visit_classes_depth_first():
+                if human_name(cls) in processed:
                     continue
-                processed.add(human_name(recipe))
-                json_config_file += '\t"is_ingredient_of_{}": '.format(name_to_snake_case(human_name(recipe))) + '{\n\t\t'
-                json_config_file += '"init_prompts": [\n\t\t\t'
-                json_config_file += '"<ENT0> is ingredient of {} ."\n\t\t'.format(human_name(recipe))
-                json_config_file += "],\n"
-                json_config_file += '\t\t"seed_ent_tuples": [\n'
-                json_config_file += "\t\t]\n"
-                json_config_file += '\t},\n'
-        # remove the last comma
-        json_config_file = json_config_file[:-2] + "\n"
-        json_config_file += '}'
+                if not subtype(cls, Recipe):
+                    continue
+                processed.add(human_name(cls))
+                # Retrieve all the recipes for the class
+                instances = list(cls.instances())
+                for recipe in instances:
+                    if human_name(recipe) in processed:
+                        continue
+                    processed.add(human_name(recipe))
+                    tmp_json_config_file = '\t"is_ingredient_of_{}": '.format(name_to_snake_case(human_name(recipe))) + '{\n\t\t'
+                    tmp_json_config_file += '"init_prompts": [\n\t\t\t'
+                    tmp_json_config_file += '"<ENT0> is ingredient of {} ."\n\t\t'.format(human_name(recipe))
+                    tmp_json_config_file += "],\n"
+
+                    # Check if the recipe is present in the auxiliary ontology
+                    # If positive add some seed entity tuples
+                    cls_aux = kg_aux.onto[human_name(recipe)]
+                    if cls_aux is not None:
+                        instances_aux = list(cls_aux.instances())
+                        if human_name(recipe) in [human_name(x) for x in instances_aux]:
+                            # Retrieve ingredients
+                            ingredients = [x for x in instances_aux if human_name(x) == "Ingredient"]
+
+
+                    json_config_file += "],\n"
+                    json_config_file += '\t\t"seed_ent_tuples": [\n'
+                    json_config_file += "\t\t]\n"
+                    json_config_file += '\t},\n'
+            # remove the last comma
+            json_config_file = json_config_file[:-2] + "\n"
+            json_config_file += '}'
 
     # Save the JSON configuration file
     with open(PATH_RELATION_INFO / "nutrition_step_2.json", "w") as f:
