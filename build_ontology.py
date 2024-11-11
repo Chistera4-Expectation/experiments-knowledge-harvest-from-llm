5d48df8d645a8a34db2786907b632262e37ccb7d
Matteo Magnini <matteo.magnini@unibo.it>
2024-11-11 14:48:19 +0100

    feat: refactored ontology generation

--- build_ontology.py
+++ build_ontology.py
@@ -1,27 +1,33 @@
 from utils import KnowledgeGraph, human_name, name_to_snake_case
 from ontologies import PATH as PATH_ONTOLOGY
 from results import PATH as PATH_RESULTS
+import fire
 import json
 import os
 
 
-PATH_RESULTS = PATH_RESULTS / "nutrition" / "100tuples_top20prompts" / "roberta-large"
+PATH_RESULTS = PATH_RESULTS
 PATH_BASE_ONTOLOGY = PATH_ONTOLOGY / "base_ontology.owl"
-PATH_POPULATED_ONTOLOGY = PATH_ONTOLOGY / "populated_ontology.owl"
 THRESHOLD = 0.02
 
 
-if __name__ == "__main__":
+def main(rel_set='nutrition',
+         model_name='roberta-large',
+         max_n_ent_tuples=100,
+         max_n_prompts=20):
+    path_results = PATH_RESULTS / rel_set / f"{max_n_ent_tuples}tuples_top{max_n_prompts}prompts" / model_name
+    path_populated_ontology = PATH_ONTOLOGY / f"{model_name}_populated.owl"
     processed_classes = set()
-    os.system("cp {} {}".format(str(PATH_BASE_ONTOLOGY), str(PATH_POPULATED_ONTOLOGY)))
-    with KnowledgeGraph(PATH_POPULATED_ONTOLOGY) as kg:
+    os.system("cp {} {}".format(str(PATH_BASE_ONTOLOGY), str(path_populated_ontology)))
+    with KnowledgeGraph(path_populated_ontology) as kg:
         Recipe = kg.onto.Recipe
         for cls in kg.visit_classes_depth_first():
             if human_name(cls) in processed_classes:
                 continue
             processed_classes.add(human_name(cls))
             # Read the results file for the specific class
-            results_file = PATH_RESULTS / "is_instance_of_{}".format(name_to_snake_case(human_name(cls))) / "ent_tuples.json"
+            results_file = path_results / "is_instance_of_{}".format(
+                name_to_snake_case(human_name(cls))) / "ent_tuples.json"
             # Iterate over the results file
             with open(results_file, "r") as f:
                 results = json.load(f)
@@ -37,3 +43,7 @@ if __name__ == "__main__":
                             processed_entities.add(ent[0])
         # Save the populated ontology
         kg.save()
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
--- build_ontology_step_2.py
+++ build_ontology_step_2.py
@@ -1,20 +1,24 @@
-from utils import KnowledgeGraph, human_name, name_to_snake_case, subtype, get_filtered_instances
+from utils import KnowledgeGraph, human_name, subtype, get_filtered_instances
 from ontologies import PATH as PATH_ONTOLOGY
 from results import PATH as PATH_RESULTS
+import fire
 import json
 import os
 
 
-PATH_RESULTS = PATH_RESULTS / "nutrition_step_2" / "10000tuples_top20prompts" / "roberta-large"
-PATH_POPULATED_ONTOLOGY = PATH_ONTOLOGY / "populated_ontology.owl"
-PATH_FINAL_ONTOLOGY = PATH_ONTOLOGY / "final_ontology.owl"
 THRESHOLD = 0.00001
 
 
-if __name__ == "__main__":
+def main(rel_set='nutrition_step_2',
+         model_name='roberta-large',
+         max_n_ent_tuples=10000,
+         max_n_prompts=20):
+    path_results = PATH_RESULTS / rel_set / f"{max_n_ent_tuples}tuples_top{max_n_prompts}prompts" / model_name
+    path_populated_ontology = PATH_ONTOLOGY / f"{model_name}_populated.owl"
+    path_final_ontology = PATH_ONTOLOGY / f"{model_name}_populated_final.owl"
     processed_classes = set()
     processed_recipes = set()
-    results_file = PATH_RESULTS / "is_ingredient_of" / "ent_tuples.json"
+    results_file = path_results / "is_ingredient_of" / "ent_tuples.json"
     recipes: dict[str: list[str]] = {}
     with open(results_file, "r") as f:
         results = json.load(f)
@@ -26,8 +30,8 @@ if __name__ == "__main__":
             if ent1 not in recipes:
                 recipes[ent1] = []
             recipes[ent1].append(ent0)
-    os.system("cp {} {}".format(str(PATH_POPULATED_ONTOLOGY), str(PATH_FINAL_ONTOLOGY)))
-    with KnowledgeGraph(PATH_FINAL_ONTOLOGY) as kg:
+    os.system("cp {} {}".format(str(path_populated_ontology), str(path_final_ontology)))
+    with KnowledgeGraph(path_final_ontology) as kg:
         Recipe = kg.onto.Recipe
         for cls in kg.visit_classes_depth_first():
             if not subtype(cls, Recipe):
@@ -36,7 +40,7 @@ if __name__ == "__main__":
                 continue
             processed_classes.add(human_name(cls))
             # Retrieve all the recipes for the class
-            instances = list(get_filtered_instances(cls, PATH_FINAL_ONTOLOGY.name[:-4]))
+            instances = list(get_filtered_instances(cls, path_final_ontology.name[:-4]))
             for recipe in instances:
                 if human_name(recipe) in recipes.keys() and len(recipes[human_name(recipe)]) > 0:
                     processed_recipes.add(human_name(recipe))
@@ -52,3 +56,7 @@ if __name__ == "__main__":
         # Save the populated ontology
         print("Processed {} recipes".format(len(processed_recipes)))
         kg.save()
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
